3b4f5b54097b59de7609b5451c07a90f623b512d
dlcastra <1dlcastra@gmail.com>
2024-08-11 18:48:20 +0200

    CustomObtainToken

--- usersapi/models.py
+++ usersapi/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from rest_framework.authtoken.models import Token
 
-# Create your models here.
+
+class CustomObtainToken(Token):
+    user_agent = models.CharField(max_length=255)
+    ip_address = models.GenericIPAddressField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.user} - {self.key}"
