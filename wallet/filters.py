0a9bacd2d426a8d5be8cc7ebfb45b206af657b97
dlcastra <1dlcastra@gmail.com>
2024-09-04 13:04:48 +0200

    filter fix

--- wallet/filters.py
+++ wallet/filters.py
@@ -1,18 +1,11 @@
 from django_filters import rest_framework as rest_filters
 
-# from usersapi.filters import DateContainsFilter
+from usersapi.filters import DateContainsFilter
 from wallet.models import WalletToWalletTransaction
 
 
-class DateContainsFilter(rest_filters.CharFilter):
-    def filter(self, qs, value):
-        if not value:
-            return qs
-        return qs.filter(timestamp__contains=value)
-
-
 class TransactionsFilter(rest_filters.FilterSet):
-    # created_at = DateContainsFilter(field_name="timestamp")
+    timestamp = DateContainsFilter(field_name="timestamp")
 
     class Meta:
         model = WalletToWalletTransaction
