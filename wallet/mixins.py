8b20e79e55809b4f0ef7946b69309316483b055f
dlcastra <1dlcastra@gmail.com>
2024-09-04 13:35:13 +0200

    WalletTransactionMixin

---
+++ wallet/mixins.py
@@ -0,0 +1,75 @@
+from decimal import Decimal, InvalidOperation
+
+from django.db import transaction
+from django.db.utils import IntegrityError
+from django.utils import timezone
+from rest_framework import status
+from rest_framework.response import Response
+
+from usersapi.models import CustomUser
+from wallet.models import Wallet, WalletToWalletTransaction
+from wallet.utils import encrypt_data
+
+
+class WalletTransactionMixin:
+    def _error_response(self, message):
+        return Response({"error": message}, status=status.HTTP_400_BAD_REQUEST)
+
+    def _get_wallet_from_user(self, user):
+        try:
+            return Wallet.objects.get(user=user)
+        except Wallet.DoesNotExist:
+            return None
+
+    def _get_wallet_to_and_user_to(self, wallet_addr_to):
+        try:
+            wallet_to = Wallet.objects.get(address=wallet_addr_to)
+            user_to = CustomUser.objects.get(id=wallet_to.user_id)
+            return wallet_to, user_to
+
+        except (Wallet.DoesNotExist, IntegrityError):
+            return None, None
+
+    def _validate_amount(self, amount):
+        try:
+            amount = Decimal(amount)
+            if amount <= 0:
+                return None
+            return amount
+
+        except (ValueError, TypeError, InvalidOperation):
+            return None
+
+    def _perform_transaction(self, user_from, user_to, wallet_from, wallet_to, amount):
+        try:
+            with transaction.atomic():
+                wallet_from.wallet_balance -= amount
+                wallet_to.wallet_balance += amount
+                wallet_from.save()
+                wallet_to.save()
+
+                WalletToWalletTransaction.objects.create(
+                    user_from=user_from,
+                    user_to=user_to,
+                    wallet_addr_from=encrypt_data(wallet_from.address),
+                    wallet_addr_to=encrypt_data(wallet_to.address),
+                    amount=amount,
+                )
+        except Exception as e:
+            transaction.rollback()
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+    def _check_transaction_duplicate(self, user_from, user_to, wallet_from, wallet_to, amount):
+        recent_transaction = (
+            WalletToWalletTransaction.objects.filter(
+                user_from=user_from,
+                user_to=user_to,
+                wallet_addr_from=encrypt_data(wallet_from.address),
+                wallet_addr_to=encrypt_data(wallet_to.address),
+                amount=amount,
+            )
+            .order_by("-timestamp")
+            .first()
+        )
+        if recent_transaction and (timezone.now() - recent_transaction.timestamp).total_seconds() < 60:
+            self._error_response("Duplicate transaction detected. Please wait before retrying.")
--- wallet/views.py
+++ wallet/views.py
@@ -1,8 +1,3 @@
-from decimal import Decimal, InvalidOperation
-
-from django.db import transaction
-from django.db.utils import IntegrityError
-from django.utils import timezone
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import permissions, status, generics, filters
 from rest_framework.permissions import IsAuthenticated
@@ -13,10 +8,10 @@ from rest_framework.viewsets import GenericViewSet
 from usersapi.models import CustomUser
 from wallet.constants import MAX_TRANSACTION_AMOUNT, MIN_TRANSACTION_AMOUNT
 from wallet.filters import TransactionsFilter
+from wallet.mixins import WalletTransactionMixin
 from wallet.models import Wallet, WalletToWalletTransaction
 from wallet.paginations import TransactionPagination
 from wallet.serializers import TransactionHistorySerializer
-from wallet.utils import encrypt_data
 
 """ --- WALLET --- """
 
@@ -73,7 +68,7 @@ class GetWalletTransactionHistoryView(generics.ListAPIView, GenericViewSet):
         return transactions
 
 
-class WalletToWallerTransactionView(APIView):
+class WalletToWallerTransactionView(WalletTransactionMixin, APIView):
     permission_classes = [IsAuthenticated]
 
     def post(self, request):
@@ -122,65 +117,3 @@ class WalletToWallerTransactionView(APIView):
             {"message": "Transaction was successful", "Your balance": wallet_from.wallet_balance},
             status=status.HTTP_201_CREATED,
         )
-
-    def _error_response(self, message):
-        return Response({"error": message}, status=status.HTTP_400_BAD_REQUEST)
-
-    def _get_wallet_from_user(self, user):
-        try:
-            return Wallet.objects.get(user=user)
-        except Wallet.DoesNotExist:
-            return None
-
-    def _get_wallet_to_and_user_to(self, wallet_addr_to):
-        try:
-            wallet_to = Wallet.objects.get(address=wallet_addr_to)
-            user_to = CustomUser.objects.get(id=wallet_to.user_id)
-            return wallet_to, user_to
-
-        except (Wallet.DoesNotExist, IntegrityError):
-            return None, None
-
-    def _validate_amount(self, amount):
-        try:
-            amount = Decimal(amount)
-            if amount <= 0:
-                return None
-            return amount
-
-        except (ValueError, TypeError, InvalidOperation):
-            return None
-
-    def _perform_transaction(self, user_from, user_to, wallet_from, wallet_to, amount):
-        try:
-            with transaction.atomic():
-                wallet_from.wallet_balance -= amount
-                wallet_to.wallet_balance += amount
-                wallet_from.save()
-                wallet_to.save()
-
-                WalletToWalletTransaction.objects.create(
-                    user_from=user_from,
-                    user_to=user_to,
-                    wallet_addr_from=encrypt_data(wallet_from.address),
-                    wallet_addr_to=encrypt_data(wallet_to.address),
-                    amount=amount,
-                )
-        except Exception as e:
-            transaction.rollback()
-            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
-
-    def _check_transaction_duplicate(self, user_from, user_to, wallet_from, wallet_to, amount):
-        recent_transaction = (
-            WalletToWalletTransaction.objects.filter(
-                user_from=user_from,
-                user_to=user_to,
-                wallet_addr_from=encrypt_data(wallet_from.address),
-                wallet_addr_to=encrypt_data(wallet_to.address),
-                amount=amount,
-            )
-            .order_by("-timestamp")
-            .first()
-        )
-        if recent_transaction and (timezone.now() - recent_transaction.timestamp).total_seconds() < 60:
-            self._error_response("Duplicate transaction detected. Please wait before retrying.")
