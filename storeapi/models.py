e1196dd8603b8bde3030658f8c74a85c1a311857
dlcastra <1dlcastra@gmail.com>
2024-09-04 13:36:19 +0200

    Unnecessary models have been removed

--- storeapi/models.py
+++ storeapi/models.py
@@ -1,26 +1,12 @@
 from django.db import models
 
-from usersapi.models import CustomUser
+from core import settings
 
 
 class Product(models.Model):
     image = models.ImageField(upload_to="products_images/", blank=True)
     name = models.CharField(max_length=127)
     description = models.TextField()
-    owner = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=12, decimal_places=2)
     release_data = models.DateTimeField(auto_now=True)
-
-
-class Order(models.Model):
-    client = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    is_paid = models.BooleanField(default=False)
-    order_id = models.CharField(max_length=100, unique=True, null=False)
-    invoice_url = models.CharField(max_length=100, null=False)
-    status = models.CharField(max_length=100, null=False, default="unpaid")
-
-
-class OrderQuantity(models.Model):
-    quantity = models.PositiveIntegerField(default=1)
-    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="orders")
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="oq_products")
