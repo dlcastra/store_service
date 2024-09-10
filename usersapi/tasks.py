d42709e770c7fde9be0f9dafbf831abecebc6386
dlcastra <1dlcastra@gmail.com>
2024-09-10 16:40:07 +0200

    Celery, RabbitMQ and sending emails

--- usersapi/tasks.py
+++ usersapi/tasks.py
@@ -8,7 +8,7 @@ def send_registration_email(email, context):
     send_mail(
         subject="Registration complete",
         message=f"Hi {context['username']}, welcome to NFT shop. Login to receive your authorization token",
-        from_email="governmentguardian0@gmail.com",  # config("EMAIL_HOST_APP"),
+        from_email=config("EMAIL_HOST_APP"),
         recipient_list=[email],
         fail_silently=False,
     )
